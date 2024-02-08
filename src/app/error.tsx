e8914491ad8b1ddfd24c157adaa3e965990f3e5f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-08 10:27:47 +0900

    🐛 /signUp・/login時にはログインページにリダイレクトするよう修正

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,8 @@
 import { FC, useEffect } from 'react'
 
 import { ErrorState } from '@/components/ErrorState'
+import { redirect, usePathname } from 'next/navigation'
+import { authRoutes } from '../constants/route'
 
 interface ErrorStateProps {
   error: Error
@@ -10,9 +12,16 @@ interface ErrorStateProps {
 }
 
 const ErrorPage: FC<ErrorStateProps> = ({ error, reset }) => {
+  const pathname = usePathname()
+  const isAuthRoute = authRoutes.includes(pathname)
+
   useEffect(() => {
     console.error(error)
   }, [error])
+
+  if (isAuthRoute) {
+    return redirect('/login')
+  }
 
   return <ErrorState showReset={true} onClick={reset} />
 }
