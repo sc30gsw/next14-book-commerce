df874b47eea80f44c331bf0c146620cd303ec5cb
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-08 10:27:15 +0900

    ✨ ログイン機能の実装

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,13 +1,19 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useTransition } from 'react'
 
+import { useProviderSignIn } from '@/hooks/useProviderSignIn'
+import { handleError } from '@/libs/utils'
+import { LoginFormInput, loginFormSchema } from '@/types/schemas/LoginFormInput'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { signIn } from 'next-auth/react'
 import Link from 'next/link'
-import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 import { FaGithub } from 'react-icons/fa'
+import { IoMdSend } from 'react-icons/io'
 import { tv } from 'tailwind-variants'
-import { LoginFormInput, loginFormSchema } from '../../schemas/LoginFormInput'
 import { Button } from '../Button'
 import { Input } from '../Input'
 
@@ -22,19 +28,55 @@ const loginForm = tv({
 export const LoginForm = () => {
   const { base, linkWrapper, link } = loginForm()
 
-  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  const [isPending, startTransition] = useTransition()
+
+  const { isLoading, providerSignIn } = useProviderSignIn()
 
   const {
     handleSubmit,
     control,
-    formState: { isSubmitting, errors },
+    formState: { errors },
     reset,
   } = useForm<LoginFormInput>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: { email: '', password: '' },
   })
 
-  const onSubmit = () => {}
+  const onSubmit: SubmitHandler<LoginFormInput> = (data) => {
+    startTransition(async () => {
+      const result = loginFormSchema.safeParse(data)
+
+      if (!result.success) {
+        toast.error(result.error.message)
+        return
+      }
+
+      const { email, password } = data
+      try {
+        const res = await signIn('credentials', {
+          email,
+          password,
+          redirect: false,
+          callbackUrl: '/',
+        })
+
+        if (!res?.ok) {
+          toast.error(res?.error || 'ログインに失敗しました')
+          return
+        }
+
+        toast.success('ログインに成功しました')
+        reset()
+        router.push('/')
+      } catch (error) {
+        handleError(error)
+
+        toast.error('ログインに失敗しました')
+      }
+    })
+  }
 
   return (
     <>
@@ -47,7 +89,7 @@ export const LoginForm = () => {
           error={errors.email?.message}
           label="メールアドレス"
           placeholder="Email"
-          disabled={isSubmitting}
+          disabled={isPending || isLoading}
         />
         <Input
           id="password"
@@ -57,16 +99,26 @@ export const LoginForm = () => {
           error={errors.password?.message}
           label="パスワード"
           placeholder="Password"
-          disabled={isSubmitting}
+          disabled={isPending || isLoading}
         />
         <div className="w-full mt-4 space-y-4">
-          <Button type="submit" text="ログイン" disabled={isSubmitting} />
+          <Button
+            type="submit"
+            submitIcon={IoMdSend}
+            text="ログイン"
+            disabled={isPending || isLoading}
+            m="l"
+            loading={isPending || isLoading}
+          />
           <Button
             type="button"
             icon={FaGithub}
             text="Githubでログイン"
-            disabled={isLoading}
+            disabled={isPending || isLoading}
+            onClick={() => providerSignIn('github')}
             color="secondary"
+            m="r"
+            loading={isPending || isLoading}
           />
         </div>
       </form>
