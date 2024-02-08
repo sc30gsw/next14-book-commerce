476568b686155570a9deb677db8d9372344c5391
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-08 09:04:10 +0900

    🚸 フォーム送信時にローディングがヒョじされるよう修正・アイコン追加

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 import React, { ComponentProps, FC } from 'react'
 import { IconType } from 'react-icons'
+import { AiOutlineLoading } from 'react-icons/ai'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const buttonStyles = tv({
-  base: 'rounded flex items-center justify-center w-full font-bold py-4 px-8',
+  base: 'rounded flex items-center justify-center w-full font-bold',
   variants: {
     color: {
       primary: 'bg-blue-500 hover:bg-blue-700 text-white',
@@ -12,35 +13,69 @@ const buttonStyles = tv({
     disabled: {
       true: 'bg-gray-500 opacity-50 cursor-not-allowed',
     },
+    size: {
+      sm: 'py-2 px-4',
+      md: 'py-4 px-8',
+    },
   },
   defaultVariants: {
     color: 'primary',
+    size: 'md',
+  },
+})
+
+const iconStyles = tv({
+  variants: {
+    m: {
+      r: 'mr-2',
+      l: 'ml-2',
+    },
+    loading: {
+      true: 'animate-spin',
+    },
   },
 })
 
 type ButtonProps = {
   type: ComponentProps<'button'>['type']
   icon?: IconType
+  submitIcon?: IconType
   text: string
   disabled?: boolean
-} & VariantProps<typeof buttonStyles>
+  onClick?: ComponentProps<'button'>['onClick']
+} & VariantProps<typeof buttonStyles> &
+  VariantProps<typeof iconStyles>
 
 export const Button: FC<ButtonProps> = ({
   type,
   // biome-ignore lint/style/useNamingConvention: icon is a component
   icon: Icon,
+  // biome-ignore lint/style/useNamingConvention: icon is a component
+  submitIcon: SubmitIcon,
   text,
   disabled,
+  onClick,
   color,
+  size,
+  m,
+  loading,
 }) => {
   return (
     <button
       disabled={disabled}
       type={type}
-      className={buttonStyles({ color, disabled })}
+      className={buttonStyles({ color, disabled, size })}
+      onClick={onClick}
     >
-      {Icon && <Icon size={24} className="mr-2" />}
+      {Icon && <Icon size={24} className={iconStyles({ m })} />}
       <span>{text}</span>
+      {SubmitIcon && <SubmitIcon size={24} className={iconStyles({ m })} />}
+      {loading && (
+        <AiOutlineLoading
+          size={24}
+          className={iconStyles({ m: 'l', loading })}
+        />
+      )}
     </button>
   )
 }
