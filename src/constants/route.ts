d1aa077783517a2a6e396595cc01bac3f06196cf
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-09 18:32:34 +0900

    🐛 apiアクセス時にミドルウェアエラーとなるため修正

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,3 +1,3 @@
 export const publicRoutes: string[] = ['/']
 export const authRoutes = ['/signUp', '/login'] satisfies string[]
-export const apiAuthPrefix = '/api/auth' satisfies string
+export const apiAuthPrefix = '/api' satisfies string
