bde36388745b68bb7f536857cea966937f4f18d4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-10 17:40:23 +0900

    🎨 tailwind-variantsを使用するよう変更

--- src/components/PurchaseProduct.tsx
+++ src/components/PurchaseProduct.tsx
@@ -2,25 +2,36 @@ import { BookType } from '@/types/Book'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { FC } from 'react'
+import { tv } from 'tailwind-variants'
+
+const purchaseProduct = tv({
+  slots: {
+    link: 'cursor-pointer shadow-2xl',
+    image: 'rounded-t-md',
+    contents: 'px-4 py-4 bg-slate-100 rounded-b-md w-[450px]',
+    title: 'text-lg font-semibold whitespace-normal',
+    price: 'mt-2 text-md text-slate-700',
+  },
+})
 
 type PurchaseProductProps = Readonly<{ book: BookType }>
 
 export const PurchaseProduct: FC<PurchaseProductProps> = ({ book }) => {
+  const { link, image, contents, title, price } = purchaseProduct()
+
   return (
-    <Link href={`/book/${book.id}`} className="cursor-pointer shadow-2xl">
+    <Link href={`/book/${book.id}`} className={link()}>
       <Image
         priority={true}
         src={book.thumbnail?.url || ''}
         alt={book.title}
         width={450}
         height={350}
-        className="rounded-t-md"
+        className={image()}
       />
-      <div className="px-4 py-4 bg-slate-100 rounded-b-md w-[450px]">
-        <h2 className="text-lg font-semibold whitespace-normal">
-          {book.title}
-        </h2>
-        <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
+      <div className={contents()}>
+        <h2 className={title()}>{book.title}</h2>
+        <p className={price()}>値段：{book.price}円</p>
       </div>
     </Link>
   )
