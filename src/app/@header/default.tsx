91710c1b65820fd05c13a62fa6642c73728cd941
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-02-13 14:35:50 +0900

    🎨 default.tsxにまとめる

---
+++ src/app/@header/default.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '@/components/Header'
+
+const DefaultHeader = () => {
+  return <Header />
+}
+
+export default DefaultHeader
--- src/app/@header/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Header } from '@/components/Header'
-import React from 'react'
-
-const HeaderPage = async () => {
-  return <Header />
-}
-
-export default HeaderPage
